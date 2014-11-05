7fecd232e42dc11e54b60c10746be10c6e31aaee
Nick Nance <nance.nick@gmail.com>
2014-11-04 22:14:39 -0600

    initial commit
    vagrant setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get -y update
+sudo apt-get -y install rabbitmq-server
+sudo apt-get -y install software-properties-common python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get -y update
+sudo apt-get -y install nodejs
+sudo npm install -g forever
