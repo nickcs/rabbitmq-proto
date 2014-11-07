bc060e60e499a76895a63f839ab67c31924becb2
Nick Nance <nick.nance@connectsolutions.com>
2014-11-06 20:49:08 -0600

    update server to automatically connect to the queue

--- index.js
+++ index.js
@@ -16,24 +16,9 @@ app.set('port', process.env.PORT || 3000)
 
 app.use(bodyParser.json())
 
-app.post('/start-server', function(req, res){
-  app.amqpConnection = amqp.createConnection({ host: 'localhost' })
-  // Wait for connection to become established.
-  app.amqpConnection.on('ready', function () {
-    status.connection = 'Connection is ready';
-
-    var options = {
-      type: 'fanout',
-      autoDelete: false
-    }
-
-    app.amqpConnection.exchange('logs', options, function(exchange){
-      app.amqpExchange = exchange;
-      status.exchange = 'Exchange is ready';
-      res.send(status);
-    });
-  });
-});
+app.get('/status', function(req, res){
+    res.send(status)
+})
 
 app.post('/emit-log', function(req, res){
   var newMessage = req.body;
@@ -41,8 +26,25 @@ app.post('/emit-log', function(req, res){
   console.log('log message level: ' + newMessage.level + ' body: ' + newMessage.message);
   app.amqpExchange.publish('', newMessage.message);
   res.send('message sent');
-});
+})
+
+app.amqpConnection = amqp.createConnection({ host: 'localhost' })
+// Wait for connection to become established.
+app.amqpConnection.on('ready', function () {
+  status.connection = 'Connection is ready';
+
+  var options = {
+    type: 'fanout',
+    autoDelete: false
+  }
+
+  // wait for exchange to be created
+  app.amqpConnection.exchange('logs', options, function(exchange){
+    app.amqpExchange = exchange;
+    status.exchange = 'Exchange is ready';
+  })
+})
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('RabbitMQ + Node.js app is running on port ' + app.get('port') + '. To exit press CTRL+C');
-});
+})
