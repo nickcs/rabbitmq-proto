f401fe6c8076dedf9a08b3f5f309fd8eefa66c40
Nick Nance <nick.nance@connectsolutions.com>
2014-11-13 17:29:07 -0600

    make gateway-sender ms fully configurable

--- gateway-sender/index.js
+++ gateway-sender/index.js
@@ -1,38 +1,33 @@
-var config = require('config')
-var amqp = require('amqp')
-var request = require('request')
+var config = require('config'),
+    amqp = require('amqp'),
+    request = require('request')
+
+var exchangeOptions = config.get('exchangeOptions'),
+    subscriptionName = process.env.SUBNAME || config.get('subscriptionName')
 
 var connection = amqp.createConnection({host: 'localhost'})
 
 connection.on('ready', function(){
-  var options = {
-    type: 'fanout',
-    autoDelete: false
-  }
+  connection.exchange(subscriptionName, exchangeOptions, function(exchange){
+    connection.queue('gateway-sender', {exclusive: true}, function(queue){
+      queue.bind(subscriptionName, '')
+      console.log(' [*] Waiting for messages on ' + subscriptionName + '. To exit press CTRL+C')
 
-  var inboundExchange = config.get('inboundExchange')
-  if (inboundExchange && inboundExchange.length > 0) {
-    connection.exchange(inboundExchange, options, function(exchange){
-      connection.queue('gateway-sender', {exclusive: true}, function(queue){
-        queue.bind(inboundExchange, '')
-        console.log(' [*] Waiting for messages on ' + inboundExchange + '. To exit press CTRL+C')
-
-        queue.subscribe(function(msg){
-          console.log(' [x] Message received');
-          request({
-            method: 'POST',
-            uri: config.get('endpoint'),
-            json: true,
-            body: msg
-          }, function(err,res,body){
-            if (err) {
-              console.log(' [!] Error sending message: ' + err);
-            } else {
-              console.log(' [X] Message sent');
-            }
-          })
+      queue.subscribe(function(msg){
+        console.log(' [x] Message received');
+        request({
+          method: 'POST',
+          uri: config.get('endpoint'),
+          json: true,
+          body: msg
+        }, function(err,res,body){
+          if (err) {
+            console.log(' [!] Error sending message: ' + err);
+          } else {
+            console.log(' [X] Message sent');
+          }
         })
       })
     })
-  }
+  })
 })
